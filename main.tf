158afb485735569307de9fedd1954f0d4e73473b
Dennis <dennisokaforab@yahoo.com>
2026-05-27 13:13:22 +0100

    add aws provider and .gitignore

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "my-demo-bucket-12345"
+  bucket = "dennis-demo-bucket-123456"
 }
